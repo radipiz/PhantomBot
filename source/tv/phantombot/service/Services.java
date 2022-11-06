d4aef4d05f20c9e25c64128f61b8477d291b1b7c
Radipiz <radipizmail@gmail.com>
2022-11-06 23:18:59 +0100

    Added possibility to use a text2speech service and send it to alertspolls ws (not used yet) First service implemented is a wrapper around GameTTSserv

---
+++ source/tv/phantombot/service/Services.java
@@ -0,0 +1,33 @@
+package tv.phantombot.service;
+
+import tv.phantombot.CaselessProperties;
+import tv.phantombot.scripts.handler.text2speech.GameTtsImpl;
+import tv.phantombot.scripts.handler.text2speech.Text2SpeechProvider;
+
+public class Services {
+    public static final String CONFIG_PREFIX_TEXT2SPEECH = "services.text2speech.";
+    public static final String CONFIG_KEY_PROVIDER = "provider";
+    private static Text2SpeechProvider tts;
+
+    private static Text2SpeechProvider createText2Speech() throws ServiceNotConfiguredException, ServiceConfigurationIncompleteException {
+        String provider = CaselessProperties.instance().getProperty(CONFIG_PREFIX_TEXT2SPEECH + ".provider", "");
+        Text2SpeechProvider ttsProvider;
+        com.gmt2001.Console.debug.println(String.format("Trying to configure text2speech provider. '%s'", provider));
+        switch(provider){
+            case GameTtsImpl.PROVIDER_NAME:
+                ttsProvider = new GameTtsImpl();
+                break;
+            default:
+                throw new ServiceNotConfiguredException("Text2Speech service is not configured. Please configure add services.text2speech.provider to config");
+        }
+        return ttsProvider;
+    }
+
+    public static Text2SpeechProvider getText2Speech() throws ServiceNotConfiguredException, ServiceConfigurationIncompleteException {
+        if(tts == null){
+            tts = createText2Speech();
+        }
+        return tts;
+    }
+
+}
